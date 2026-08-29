d7292d86d6e6f3b16f4008bc5336e179fc0f584b
Nicolas <nicolaskennedy619@gmail.com>
2026-08-29 05:16:25 -0300

    Finalizado com sucesso exercício 6

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,26 @@
-const repeatString = function() {
 
+
+const repeatString = function(word, number) {
+
+    let text = "";
+
+    while (true) {
+
+        if (Number(number) < 0) {
+            return "ERROR";
+        }
+
+        else {
+
+            for(let i = 0; i < Number(number); i++) {
+                text += String(word)
+            };
+            return text;
+            // return String(word).repeat(number);
+        }
+
+    }
+    
 };
 
 // Do not edit below this line
