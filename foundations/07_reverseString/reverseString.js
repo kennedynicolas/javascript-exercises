b3e30df447e8c2a2f0a98fd00155b8237149b5c2
Nicolas <nicolaskennedy619@gmail.com>
2026-08-29 05:41:17 -0300

    Finalizado com sucesso o exercício 7

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,23 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let text = "";
 
+    for (let i = word.length - 1; i >= 0; i--) {
+        text += word.at(i)
+    }
+    return text
 };
+
+// let word = "hello";
+// let tamanho = word.length;
+// let resultado = "";
+
+// for (let i = -1; i > -tamanho-1; i--) {
+//         resultado += word.at(i);
+//     }
+
+// console.log(resultado);
+
+// console.log(reverseString("Hello"))
 
 // Do not edit below this line
 module.exports = reverseString;
