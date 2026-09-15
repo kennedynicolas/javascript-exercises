9dea5357d06edf53cc23bd423eb861ec0c047bc6
Nicolas <nicolaskennedy619@gmail.com>
2026-09-15 17:48:42 -0300

    Finalizado exercício 9

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
 
+    if (!Number.isInteger(x) || !Number.isInteger(y) || x < 0 || y < 0) {
+        return "ERROR";
+    }
+
+    console.log(x, y)
+
+    const min = Math.min(x, y);
+    const max = Math.max(x, y);
+
+    let num = 0
+    for (let i = min; i <= max; i++) {
+        num += i;
+    }
+
+    return num;
 };
+
+console.log(sumAll(123, -1))
 
 // Do not edit below this line
 module.exports = sumAll;
